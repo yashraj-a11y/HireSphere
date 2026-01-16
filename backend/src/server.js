6885d659d024cd49ca7741668c16996495281674
yashraj-a11y <rajy18594@gmail.com>
2026-01-16 13:25:24 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,14 +5,36 @@
 import express from "express" 
 const app = express()
 import {ENV} from "./lib/env.js"
+import path from "path"
+
+
+const __dirname = path.resolve()
 
 
 
 
-app.get('/help',(req,res) => {
+app.get('/health',(req,res) => {
     res.status(200).json({"msg" : "api is up and running"})
-})
+}) ;
 
+
+app.get('/books',(req,res) => {
+    res.status(200).json({"msg" : "this is books endpoint"})
+}) ;
+
+// make our app ready for deployment
+
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"../frontend/dist"))) ;
+
+    app.get("/{*any}" , (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend", "dist" , "index.html"))
+
+    }) ;
+
+
+
+}
 
 
 
